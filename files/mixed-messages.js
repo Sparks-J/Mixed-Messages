9f79dad8bb5fba11dcd46ce545729d7fc8c4350f
Sparks-J <josh.sparks.95@gmail.com>
2022-08-31 21:23:46 -0400

    fixed some characters not displaying correctly

--- files/mixed-messages.js
+++ files/mixed-messages.js
@@ -1,5 +1,5 @@
 const quote = ['Fortune favors the bold', 'I think, therefore I am', 'Time is money', 'I came, I saw, I conquered', "Life would be tragic if it weren't funny", "There's a sucker born every minute", 'In the middle of difficulty lies opportunity']
-const author = ['Virgil', 'Rene Descartes', 'Benjamin Franklin', 'Julius Caesar', 'Stephen Hawking', 'P.T. Barnum', 'Albert Einstein']
+const author = ['Virgil', 'René Descartes', 'Benjamin Franklin', 'Julius Caesar', 'Stephen Hawking', 'P.T. Barnum', 'Albert Einstein']
 const disclaimer = ['maybe', 'probably', 'I think', 'or something like that', 'yeah, definitely']
 
 function mixedMessage(){
