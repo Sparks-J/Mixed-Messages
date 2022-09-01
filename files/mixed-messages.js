2b1b182393d508efc330a50e4c35e6e861aad63a
Sparks-J <josh.sparks.95@gmail.com>
2022-08-31 21:40:05 -0400

    added more quotes

--- files/mixed-messages.js
+++ files/mixed-messages.js
@@ -1,6 +1,29 @@
-const quote = ['Fortune favors the bold', 'I think, therefore I am', 'Time is money', 'I came, I saw, I conquered', "Life would be tragic if it weren't funny", "There's a sucker born every minute", 'In the middle of difficulty lies opportunity']
-const author = ['Virgil', 'René Descartes', 'Benjamin Franklin', 'Julius Caesar', 'Stephen Hawking', 'P.T. Barnum', 'Albert Einstein']
-const disclaimer = ['maybe', 'probably', 'I think', 'or something like that', 'yeah, definitely']
+const quote = [
+    'Fortune favors the bold',
+    'I think, therefore I am',
+    'Time is money',
+    'I came, I saw, I conquered',
+    "Life would be tragic if it weren't funny",
+    "There's a sucker born every minute",
+    'In the middle of difficulty lies opportunity',
+    'The best way out is always through',
+    'I am, therefore I think']
+const author = [
+    'Virgil',
+    'René Descartes',
+    'Benjamin Franklin',
+    'Julius Caesar',
+    'Stephen Hawking',
+    'P.T. Barnum',
+    'Albert Einstein',
+    'Robert Frost', 
+    'Ayn Rand']
+const disclaimer = [
+    'maybe',
+    'probably',
+    'I think',
+    'or was it someone else?',
+    'yeah, definitely']
 
 function mixedMessage(){
     let randQuote = quote[Math.floor(Math.random()*quote.length)]
